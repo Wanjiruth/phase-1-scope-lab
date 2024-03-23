02e94f2706c62a2baf38ce16db40ddffd8f19e5b
wanjiruth <njoki.ruth.wanjiku@gmail.com>
2024-03-23 23:38:26 +0300

    Initial

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+// Write your soluvar customerName = 'bob';
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // This will throw an error because you cannot reassign a constant variable.
+}
+
+
+    
+
+
